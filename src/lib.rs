559d0973c87fb1a9c020160fe71f60aafa1733f9
James Elderfield <jad.elderfield@gmail.com>
2020-04-04 15:11:12 +0300

    Extend core types and use a simple linkage map

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,62 @@
+extern crate readonly;
+
 use rand::prelude::*;
 
 type Locus = i32;
-pub type Chromosome = std::vec::Vec<Locus>;
+type GeneticDistance = f64;
+type LociValues = std::vec::Vec<Locus>;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub type ChromosomeMap = std::vec::Vec<GeneticDistance>;
 
-    #[test]
-    /// Tests that attempting to recombine chromosomes of unequal size gives an error
-    fn unequal_chromosome_error() {
-        let father_chromosome = vec![1, 1, 1, 0];
-        let mother_chromosome = vec![1, 0, 1];
+#[readonly::make]
+#[derive(Debug)]
+pub struct Chromosome<'a> {
+    #[readonly]
+    pub loci: LociValues,
+    map: &'a ChromosomeMap,
+}
 
-        assert!(recombine(father_chromosome, mother_chromosome).is_err());
+impl<'a> Chromosome<'a> {
+    /// Constructs a new chromosome
+    pub fn new(map: &'a ChromosomeMap) -> Self {
+        Chromosome {
+            loci: vec![Default::default(); map.len() + 1],
+            map: map,
+        }
+    }
+
+    /// Sets the loci values
+    pub fn set_loci(&mut self, loci: LociValues) -> Option<String> {
+        if self.map.len() != loci.len() - 1 {
+            return Some(format!(
+                "Cannot assign loci of size {}. Map expects {}.",
+                loci.len(),
+                self.map.len() + 1
+            ));
+        }
+
+        self.loci = loci;
+
+        None
+    }
+
+    /// Returns the number of loci
+    pub fn len(&self) -> usize {
+        self.map.len() + 1
+    }
+}
+
+impl std::ops::Index<usize> for Chromosome<'_> {
+    type Output = Locus;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.loci[index]
+    }
+}
+
+impl std::ops::IndexMut<usize> for Chromosome<'_> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.loci[index]
     }
 }
 
@@ -28,18 +71,27 @@ mod tests {
 ///
 /// ```
 /// use genedrop_core::recombine;
+/// use genedrop_core::Chromosome;
 ///
-/// let child = recombine(vec![0, 1, 2, 1], vec![0, 2, 2, 1]);
+/// let map = vec![0.5, 0.1, 0.05];
+///
+/// let mut father_chromosome = Chromosome::new(&map);
+/// father_chromosome.set_loci(vec![1, 1, 1, 0]);
+/// let mut mother_chromosome = Chromosome::new(&map);
+/// mother_chromosome.set_loci(vec![1, 0, 1, 1]);
+///
+/// let child = recombine(&father_chromosome, &mother_chromosome);
 /// ```
 pub fn recombine<'a>(
-    father_chromosome: Chromosome,
-    mother_chromosome: Chromosome,
-) -> Result<Chromosome, &'a str> {
-    if father_chromosome.len() != mother_chromosome.len() {
-        return Result::Err("Chromosomes of unequal length cannot be recombined.");
+    father_chromosome: &Chromosome<'a>,
+    mother_chromosome: &Chromosome<'a>,
+) -> Result<Chromosome<'a>, &'a str> {
+    if !std::ptr::eq(father_chromosome.map, mother_chromosome.map) {
+        return Result::Err("Chromosomes with different maps cannot be recombined.");
     }
 
-    let mut child_chromosome: Chromosome = vec![0; father_chromosome.len()];
+    // NB: It doesn't matter whether we father or mother chromosome as must be the same
+    let mut child_chromosome: Chromosome = Chromosome::new(father_chromosome.map);
 
     // Choose initial chromosome to choose from at random
     let mut choose_from: &Chromosome;
@@ -56,11 +108,33 @@ pub fn recombine<'a>(
     for i in 0..child_chromosome.len() {
         child_chromosome[i] = choose_from[i];
 
-        // TODO: Add proper genetic distance
-        if random::<f64>() < 0.5 {
+        if i < child_chromosome.len() - 1 && random::<f64>() < child_chromosome.map[i] {
             std::mem::swap(&mut choose_from, &mut other_chromosome);
         }
     }
 
     return Result::Ok(child_chromosome);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    /// Tests that attempting to recombine chromosomes of unequal size gives an error
+    fn unequal_chromosome_error() {
+        let map = vec![0.5, 0.1, 0.05];
+
+        let mut father_chromosome = Chromosome::new(&map);
+        let father_err = father_chromosome.set_loci(vec![1, 1, 1]);
+        let mut mother_chromosome = Chromosome::new(&map);
+        let mother_err = mother_chromosome.set_loci(vec![1, 0, 1, 1]);
+
+        // TODO: This is actually testing chromosome, not recombination per se
+        assert!(father_err.is_some());
+        assert!(!mother_err.is_some());
+
+        recombine(&father_chromosome, &mother_chromosome);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
-    let father_chromosome: genedrop_core::Chromosome = vec![1, 1, 1, 0];
-    let mother_chromosome: genedrop_core::Chromosome = vec![1, 0, 1, 0];
+    let map = vec![0.5, 0.1, 0.05];
 
-    let child_chromosome = genedrop_core::recombine(father_chromosome, mother_chromosome);
+    let mut father_chromosome = genedrop_core::Chromosome::new(&map);
+    father_chromosome.set_loci(vec![1, 1, 1, 0]);
+
+    let mut mother_chromosome = genedrop_core::Chromosome::new(&map);
+    mother_chromosome.set_loci(vec![1, 0, 1, 1]);
+
+    let child_chromosome = genedrop_core::recombine(&father_chromosome, &mother_chromosome);
 
     println!("Hello, world!");
     println!("{:?}", child_chromosome);
